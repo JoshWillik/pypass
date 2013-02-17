0a099e97d4f4cea1325b3ef36f90f67d013758ec
Josh(Desktop) <joshwillik@gmail.com>
2013-02-17 05:48:18 -0500

    Add support for numerical argument for length of password (in words)

--- pypass.py
+++ pypass.py
@@ -1,16 +1,20 @@
 #! /usr/bin/python
-import random
+import random, sys
+try:
+    pass_length = int(sys.argv[1])
+except:
+    pass_length = 4
 with open('/usr/share/dict/cracklib-small', 'r') as f:
 	size_count = 0
 	for line in f:
 		size_count += 1
 	wordnums = []
-	for i in range(6):
+	for i in range(pass_length):
 		wordnums.append(random.randint(0, size_count))
 	finished = ""
 	for i in wordnums:
 		f.seek(0)
 		for m in range(i):
 			f.readline()
-		finished += f.readline().strip() + "|"
+		finished += f.readline().strip().title() + "|"
 	print(finished)
